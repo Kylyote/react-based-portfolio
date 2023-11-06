59ff26943e67dfe1150debc037f3f634d81dc0b3
Kyle Jocoy <kjocoy@gmail.com>
2023-11-05 22:08:49 -0800

    adding starter code from the mini project and react router activity with some changes

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// import all the pages that react will use
+import App from './App';
+import About from './About';
+import Contact from './Contact';
+import Error from './Error';
+import Porfolio from './Portfolio';
+import Resume from './Resume';
+
+// Define the routes needed for the navbar to function
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        // Are the / needed before the path? 
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'portfolio',
+        element: <Porfolio />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
